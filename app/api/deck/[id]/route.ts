20f7e052c940a85aa13e79ef640c94aef10d32d9
YuriAtzler <atzleryuri@gmail.com>
2025-04-24 21:24:43 -0300

    refactor: ajustar funções DELETE e POST para melhorar a manipulação de IDs e status de resposta

--- app/api/deck/[id]/route.ts
+++ app/api/deck/[id]/route.ts
@@ -1,30 +1,41 @@
+import { NextRequest } from "next/server";
+
 import { deleteDeck, updateDeck } from "@/lib/queries/deck";
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = parseInt(params.id);
+export async function DELETE(req: NextRequest) {
+  const url = new URL(req.url);
+  const id = parseInt(url.pathname.split("/").pop() || "");
+
+  if (isNaN(id)) {
+    return Response.json({ message: "ID inválido" }, { status: 400 });
+  }
 
   await deleteDeck(id);
 
-  return Response.json({
-    message: "Deck deleted successfully",
-    status: 201,
-  });
+  return Response.json(
+    {
+      message: "Deck deleted successfully",
+    },
+    { status: 200 }
+  );
 }
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function POST(req: NextRequest) {
+  const url = new URL(req.url);
+  const id = parseInt(url.pathname.split("/").pop() || "");
+
+  if (isNaN(id)) {
+    return Response.json({ message: "ID inválido" }, { status: 400 });
+  }
+
   const data = await req.json();
-  const id = parseInt(params.id);
 
   await updateDeck(id, data.name, data.languageId);
 
-  return Response.json({
-    message: "Deck updated successfully",
-    status: 201,
-  });
+  return Response.json(
+    {
+      message: "Deck updated successfully",
+    },
+    { status: 200 }
+  );
 }
